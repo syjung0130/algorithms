3d4161d7f6de274558c50006bb2a51cacefed0ab
SOONYEONG JUNG <soonyoung87@gmail.com>
2021-08-29 09:41:17 +0900

    modify graph.

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -4,39 +4,6 @@
 
 using namespace std;
 
-#if 0
-       1
-     / | \
-    3  |   2
-  / |  |    \
- 4  5  |     7
-       |  /  |
-        8    6
-map<int, list<int>> graph = {
-    {0, {}},
-    {1, {2, 3, 8}},
-    {2, {1, 7}},
-    {3, {1, 4, 5}},
-    {4, {3, 5}},
-    {5, {3, 4}},
-    {6, {7}},
-    {7, {2, 6, 8}},
-    {8, {1, 7}}
-};
-#endif
-
-int getVisitedNodeNum(vector<vector<int>> &graph, vector<bool> &visited, int v) {
-    int visitedNodeNum = 0;
-
-    for (int i = 0; i < graph[v].size(); i++) {
-        if (visited[graph[v][i]] == true) {
-            visitedNodeNum++;
-        }
-    }
-
-    return visitedNodeNum;
-}
-
 void bfs(vector<vector<int>> &graph, int startNode, vector<bool> &visited) {
     cout << "bfs()" << endl;
 
@@ -60,17 +27,24 @@ void bfs(vector<vector<int>> &graph, int startNode, vector<bool> &visited) {
 }
 
 int main() {
-
+#if 0
+       1
+     / | \
+    2  3   4
+    |  
+    5  
+   / \ 
+  6   7
+#endif
     vector<vector<int>> graph = {
         {},
-        {2, 3, 8},
-        {1, 7},
-        {1, 4, 5},
-        {3, 5},
-        {3, 4},
-        {7},
-        {2, 6, 8},
-        {1, 7},
+        {2, 3, 4},  //1
+        {5},        //2
+        {5},        //3
+        {},         //4
+        {6, 7},     //5
+        {},         //6
+        {3},        //7
     };
 
     int node_num = graph.size();
