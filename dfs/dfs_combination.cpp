a3afaed4dafc9f741e70b936ced1d309c88b1a5a
SOONYEONG JUNG <soonyoung87@gmail.com>
2023-03-05 19:23:09 +0900

    Update dfs_combination.cpp

--- dfs/dfs_combination.cpp
+++ dfs/dfs_combination.cpp
@@ -1,43 +1,44 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<vector<int>> results;
 
-void dfs_list(vector<int> &elem, vector<int> &nums, int start, int k) {
+void dfs_combination(vector<int>& elem, vector<int>& nums, int start, int k) {
 	if (k == 0) {
 		results.push_back(elem);
 		return;
 	}
 	for (int i = start; i < nums.size(); i++) {
 		elem.push_back(nums[i]);
-		dfs_list(elem, nums, i+1, k-1);
+		dfs_combination(elem, nums, i + 1, k - 1);
 		elem.pop_back();
 	}
 }
 
-void combi_list(vector<int> &nums, int k) {
-	vector<int> elem;
-
-	dfs_list(elem, nums, 0, k);
+void combination(vector<int>& nums, int k) {
+	vector<int> elems;
+	dfs_combination(elems, nums, 0, k);
 }
 
 int main()
 {
-	cout << "main(): " << endl;
+	//cout << "main(): " << endl;
+
+	vector<int> nums = { 1, 2, 3, 4, 5 };
+	combination(nums, 3);
+
 	cout << "results: " << endl;
-
-	vector<int> nums = {1, 2, 3, 4, 5};
-	combi_list(nums, 3);
-
-	for (auto v: results) {
-		cout << "{";
-		for (auto d: v) {
+	cout << "{" << endl;
+	for (auto v : results) {
+		cout << "  {";
+		for (auto d : v) {
 			cout << d << ", ";
 		}
-		cout << "}," << endl;
+		cout << "}" << endl;
 	}
+	cout << "}" << endl;
 
 	return 0;
 }
